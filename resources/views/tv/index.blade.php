e2fc5bf424dc4c6490efafe52e2329bdb920b7d2
Hasan Çamözü <h.camozuu@gmail.com>
2021-03-31 17:27:21 +0300

    tv.index created

---
+++ resources/views/tv/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-tv mx-12 md:mx-0">
+            <h2 class="uppercase tracking-wider text-yellow-600 text-lg font-semibold">Popular Shows</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">  
+                @foreach ($popularShows as $tv)
+                    <x-tv-card :tv="$tv" :genres="$genres" />
+                @endforeach
+            </div>    
+        </div>
+        <div class="now-playing-tv mx-12 mt-16 md:mx-0">
+            <h2 class="uppercase tracking-wider text-yellow-600 text-lg font-semibold">Top Rated Shows</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">  
+                @foreach ($topRatedTv as $tv)
+                    <x-tv-card :tv="$tv" :genres="$genres" />
+                @endforeach
+            </div>    
+        </div>
+    </div>
+@endsection
